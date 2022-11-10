bddebaa655a92d40dbf4e1e23a11b647c62bcce2
Wen Yang <yellowriver2010@hotmail.com>
2022-11-10 11:19:53 +0800

    exp_no_3: add a delay after mounting the mmc disk to avoid switch_root failure

--- exp_no_3/_switch_root.sh
+++ exp_no_3/_switch_root.sh
@@ -3,13 +3,14 @@
 set -x
 
 /bin/mkdir /proc /sys /dev /mnt
+
 /bin/mount -t proc none /proc
 /bin/mount -t sysfs none /sys
 
 /sbin/mdev -s
+
 /bin/sleep 2
 /bin/mount /dev/mmcblk0 /mnt
 
+/bin/sleep 1
 exec switch_root /mnt /bin/sh
-
-
